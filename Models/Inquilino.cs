e87ce65c66b266a0a9d8c3137c9a9e33f0d89596
kevinorozcocarp93 <kevinorozcocarp@gmail.com>
2026-08-13 17:52:43 -0300

    commit 2 inquilino

---
+++ Models/Inquilino.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace InmobiliariaGrupo6_.Models;
+
+public class Inquilino
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string DNI { get; set; } = string.Empty;
+
+    [Required]
+    public string NombreCompleto { get; set; } = string.Empty;
+
+    public string Telefono { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
--- obj/Debug/net10.0/InmobilariaGrupo6-.AssemblyInfo.cs
+++ obj/Debug/net10.0/InmobilariaGrupo6-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db180c759472afd51a422a80e116a6ccaf6c36c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
