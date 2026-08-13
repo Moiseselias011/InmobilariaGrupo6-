38e8d3d48da9f81544750f03433d1034b027d325
Moises011 <eriannbel@gmail.com>
2026-08-13 19:49:32 -0300

    Create PropietarioControler.cs

---
+++ Controllers/PropietarioCotroler.cs
@@ -0,0 +1,23 @@
+public class PropietarioControler : controler 
+{
+   public IActionResult Index ()
+   {
+       return View ();
+   }
+   public IActionResult Create ()
+   {
+     return View ();
+   }
+   public IActionResult Edit ()
+   {
+    return View ();
+   }
+   public IActionResult Delete ()
+   {
+    return View ();
+   }
+   public IactionResult Details ()
+   {
+    return View ();
+   }
+}
