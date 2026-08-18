7c21524dec2f6de792fc68363ddcb3370df15130
Moises011 <eriannbel@gmail.com>
2026-08-18 14:32:23 -0300

    .gitignore por el cache

--- obj/Debug/net10.0/InmobilariaGrupo6-.AssemblyInfo.cs
+++ obj/Debug/net10.0/InmobilariaGrupo6-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec230da6bd5dd9eafba426347ced5f8304fafff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f912171833890f93f7ea9ece470f055993155a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobilariaGrupo6-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
